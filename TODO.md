1ec8eb4b4d2e65c411a1a8e2ec613c55abea3c36
Afonso Raposo <afonsocraposo@gmail.com>
2021-02-01 02:30:20 +0000

    Accelerometer can't keep sampling rate

--- TODO.md
+++ TODO.md
@@ -1,5 +1,6 @@
 # TODO
 
+- Fix controllers clear data
 - Avoid starting the acquisiton without sensors
 - Decimate signals to avoid drawing every point
 - Save data to csv file
--- lib/controllers/accelerometer_controller.dart
+++ lib/controllers/accelerometer_controller.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 
 import 'package:flutter_sensors/flutter_sensors.dart';
 import 'package:scientisst_journal/controllers/controller.dart';
-import 'package:scientisst_journal/data/fixed_list.dart';
 import 'package:scientisst_journal/data/sensor_options.dart';
 import 'package:scientisst_journal/data/sensor_value.dart';
 
@@ -12,17 +11,18 @@ class AccelerometerController extends Controller {
   int samplingRate;
   int _t;
   DateTime _timestamp;
-  int _n;
   List data = [];
   final SensorOptions options;
+  final int bufferSizeSeconds;
+  int _n;
 
-  AccelerometerController({this.options, bufferSizeSeconds = 5}) {
+  AccelerometerController({this.options, this.bufferSizeSeconds = 5}) {
     this.channels = List<int>.from(
       List.generate(options.channels.length,
               (index) => options.channels[index] ? index : null)
           .where((index) => index != null),
     );
-    this.samplingRate = options.samplingRate ?? 50;
+    this.samplingRate = options.samplingRate ?? 17;
 
     _t = 1000 ~/ samplingRate;
     _n = samplingRate * bufferSizeSeconds;
@@ -31,13 +31,20 @@ class AccelerometerController extends Controller {
   void start() async {
     final stream = await SensorManager().sensorUpdates(
       sensorId: Sensors.ACCELEROMETER,
-      interval: Sensors.SENSOR_DELAY_GAME,
+      interval: Sensors.SENSOR_DELAY_FASTEST,
     );
     _timestamp = DateTime.now();
     _clearData();
     subscription = stream.listen(
       (sensorEvent) {
-        _timestamp = _timestamp.add(Duration(milliseconds: _t));
+        _timestamp = DateTime.now();
+        while (data.first.isNotEmpty &&
+            _timestamp.difference(data.first.first.timestamp).inSeconds >
+                bufferSizeSeconds) {
+          for (int i = 0; i < channels.length; i++) {
+            data[i].removeAt(0);
+          }
+        }
         for (int i = 0; i < channels.length; i++) {
           data[i].add(
             SensorValue(
@@ -53,14 +60,12 @@ class AccelerometerController extends Controller {
   void _clearData() {
     data = List.generate(
       this.channels.length,
-      (_) => FixedList.from(
-        List<SensorValue>.generate(
-          _n,
-          (int index) => SensorValue(
-              DateTime.fromMillisecondsSinceEpoch(
-                  _timestamp.millisecondsSinceEpoch - (_n - 1 - index) * _t),
-              0),
-        ),
+      (_) => List<SensorValue>.generate(
+        _n,
+        (int index) => SensorValue(
+            DateTime.fromMillisecondsSinceEpoch(
+                _timestamp.millisecondsSinceEpoch - (_n - 1 - index) * _t),
+            0),
       ),
     );
   }
